73266565424dd8fec43da1890f7e94abe52c5175
ryotaro-rks <ryotaro.seya@rakus.co.jp>
2021-05-28 14:40:00 +0900

    create show article and comment.

--- src/main/java/com/example/controller/ArticleController.java
+++ src/main/java/com/example/controller/ArticleController.java
@@ -1,12 +1,22 @@
 package com.example.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.domain.Article;
 import com.example.service.ArticleService;
 import com.example.service.CommentService;
 
+/**
+ * 記事用コントローラクラス.
+ * 
+ * @author ryotaro.seya
+ *
+ */
 @Controller
 @RequestMapping("/bbs")
 public class ArticleController {
@@ -16,8 +26,31 @@ public class ArticleController {
 	@Autowired
 	private CommentService commentService;
 
+	/**
+	 * @return 掲示板ページへのフォワード
+	 */
 	@RequestMapping("")
-	public String index() {
+	public String index(Model model) {
+		List<Article> articleList = articleService.showAllArticle();
 
+		for (Article article : articleList) {
+			article.setCommentList(commentService.showAllComment(article.getId()));
+		}
+
+		articleList.forEach(System.out::println);
+
+		model.addAttribute("articleList", articleList);
+
+		return "bbs";
+	}
+
+	/**
+	 * 記事投稿
+	 * 
+	 * @return 掲示板ページ
+	 */
+	@RequestMapping("post")
+	public String post(Model model) {
+		return index(model);
 	}
 }
